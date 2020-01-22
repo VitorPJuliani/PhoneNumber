c59406a990800ccfc695dee5f0661efe6423277d
VitorPJuliani <vitor.juliani@sympla.com.br>
2020-01-22 10:23:05 -0200

    add documentation

--- src/reader/FileReader.java
+++ src/reader/FileReader.java
@@ -9,12 +9,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
+ * Class to read files
  * @author Vitor Juliani
  * @version 1.0
  */
 public class FileReader {
 
     /**
+     * Method that read a file and return the content
      * @param fileName String that represents the name of file
      * @return Return a list of Strings with all phone numbers
      */
